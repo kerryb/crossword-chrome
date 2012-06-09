f78ba4243f8f402fcee710b9b2330856ee05b206
Kerry Buckley <kerryjbuckley@gmail.com>
2012-06-09 19:49:56 +0100

    Clean up page on click

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
 function onRequest(request, sender, sendResponse) {
-  chrome.pageAction.show(sender.tab.id);
+  enable(sender.tab);
   sendResponse({});
 }
 
+function onClicked(tab) {
+  cleanPage();
+}
+
+function enable(tab) {
+  chrome.pageAction.show(tab.id);
+}
+
+function cleanPage() {
+  chrome.tabs.executeScript(null, {file: "jquery-1.7.2.min.js"});
+  chrome.tabs.executeScript(null, {file: "alterPage.js"});
+}
+
 chrome.extension.onRequest.addListener(onRequest);
+chrome.pageAction.onClicked.addListener(onClicked);
