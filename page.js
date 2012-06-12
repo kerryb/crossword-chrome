e2ff96cb974f730c7042c3dbc3daf3946d82072c
Kerry Buckley <kerryjbuckley@gmail.com>
2012-06-12 19:12:15 +0100

    Add empty chat div to page

--- page.js
+++ page.js
@@ -1,4 +1,5 @@
 function setupPage() {
   $("#box").siblings().hide();
   $(".discussion, .crossword-spoiler, .discussion-pagination").hide();
+  $("#box").after('<div id="chat"><h1>Chat</h1></div>');
 }
